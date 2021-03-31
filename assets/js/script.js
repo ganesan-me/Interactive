dc475e79a05159e97483ea4a95127ae4ff334355
Ganesan J <60636367+GanesanJeyaraman@users.noreply.github.com>
2021-03-31 22:19:29 +0530

    Update script.js
    updated scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,7 +66,7 @@ function loadDoc() {
       json = this.responseText;
     }
   };
-  xhttp.open("GET", "https://ganesan-me.github.io/Interactive/assets/js/empInfo.json", false);
+  xhttp.open("GET", `${window.location.href}assets/js/empInfo.json`, false);
   xhttp.send();
 }
 
